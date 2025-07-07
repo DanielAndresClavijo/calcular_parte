8dc4db909093cc8b3dd0b072a94971063b0f8d89
DanielAndresClavijo <danielclavijo32@gmail.com>
2025-07-07 18:25:12 -0500

    Removes Device Preview
    Removes the Device Preview package and configuration.
    
    This simplifies the application setup and improves the development
    experience by removing the dependency on Device Preview. The version is
    also incremented.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
-import 'dart:io';
-
 import 'package:calcular_parte/theme/app_theme.dart';
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'screens/report_home_page.dart';
 
 void main() {
-  runApp(DevicePreview(
-    enabled: kIsWeb || Platform.isWindows,
-    builder: (_) => const ReportApp(),
-  ));
+  runApp(const ReportApp());
 }
 
 class ReportApp extends StatelessWidget {
@@ -22,8 +15,6 @@ class ReportApp extends StatelessWidget {
       title: 'Calcular Parte',
       theme: AppTheme.theme,
       home: const ReportHomePage(),
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
     );
   }
 }
