29eb33faf6ec975bf576ac2bb43202369b003fc3
DanielAndresClavijo <danielclavijo32@gmail.com>
2025-07-07 23:21:52 -0500

    Updates app version to 2.2.0
    Updates the displayed app version number in the splash screen and the pubspec.yaml file.
    
    This reflects the latest release of the application.

--- lib/screens/splash_page.dart
+++ lib/screens/splash_page.dart
@@ -81,7 +81,7 @@ class _SplashPageState extends State<SplashPage> {
                 ),
               ),
             const SizedBox(height: 8),
-            const Text('Versión 2.1.0+100'),
+            const Text('Versión 2.2.0+100'),
             const SizedBox(height: 48),
             // Indicador de carga
             CircularProgressIndicator(
